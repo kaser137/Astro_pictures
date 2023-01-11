5a341a7d1e00c9f76fb1d03742a25b10cd634ae2
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-01-11 17:17:48 +0200

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -11,4 +11,4 @@ updates = bot.get_updates()
 print(updates)
 chat_id = '@Astro_photos'
 print(chat_id)
-bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+bot.send_message(chat_id=chat_id, text="Test text")
