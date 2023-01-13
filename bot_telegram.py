00466e802ca31c177a02706961854e7b07eaa978
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-01-13 04:04:38 +0200

    Update bot_telegram.py.

--- bot_telegram.py
+++ bot_telegram.py
@@ -4,11 +4,11 @@ import argparse
 from service_functions import output_images_to_telegram
 
 dotenv.load_dotenv('venv/.env')
-token = os.getenv('BOT_TOKEN')
-chat_id = '@Astro_photos'
+token_bot = os.getenv('BOT_TOKEN')
+chat_id = os.getenv('CHAT_ID')
 parser = argparse.ArgumentParser('This function outputs pictures to Telegram chat with period "period" in secs, '
                                  'if omitted, period = 4 hours ')
 parser.add_argument('period')
 parser = parser.parse_args()
 period = int(parser.period)
-output_images_to_telegram(token, chat_id, 'images', period=period)
+output_images_to_telegram(token_bot, chat_id, 'images', period=period)
