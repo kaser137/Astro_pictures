f179350651fa88626f3fe8ea269d2d2567d9a76a
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-01-14 04:44:50 +0200

    Update README.

--- bot_telegram.py
+++ bot_telegram.py
@@ -6,9 +6,12 @@ from service_functions import output_images_to_telegram
 dotenv.load_dotenv('venv/.env')
 token_bot = os.getenv('BOT_TOKEN')
 chat_id = os.getenv('CHAT_ID')
-parser = argparse.ArgumentParser('This function outputs pictures to Telegram chat with period "period" in secs, '
-                                 'if omitted, period = 4 hours ')
-parser.add_argument('period')
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--period', help='number of seconds', type=int, default=14400)
+
+parser.add_argument('-i', '--image', help='path/name_image', default=None)
 parser = parser.parse_args()
-period = int(parser.period)
-output_images_to_telegram(token_bot, chat_id, 'images', period=period)
+period = parser.period
+picture = parser.image
+
+output_images_to_telegram(token_bot, chat_id, 'images', period=period, picture=picture)
--- collect_pictures.py
+++ collect_pictures.py
@@ -3,7 +3,7 @@ import dotenv
 from fetch_spacex_last_launch import fetch_spacex_last_launch
 from get_nasa_pictures import get_nasa_pictures
 from get_epic_nasa import get_epic_nasa
-from service_functions import output_images_to_telegram
+
 
 dotenv.load_dotenv('venv/.env')
 id_for_example = '5eb87d47ffd86e000604b38a'
@@ -11,7 +11,6 @@ token_nasa = os.getenv('NASA_TOKEN')
 token_bot = os.getenv('BOT_TOKEN')
 chat_id = os.getenv('CHAT_ID')
 dir_pictures = 'images'
-period = input('Input period of posting (in seconds).For period 4 hours, press Enter:')
 count = input('Input number of pictures from NASA, what should you get. For 1 picture, press Enter:')
 fetch_spacex_last_launch(id_for_example)
 if count:
@@ -19,7 +18,3 @@ if count:
 else:
     get_nasa_pictures(token_nasa)
 get_epic_nasa(token_nasa)
-if period:
-    output_images_to_telegram(token_bot, chat_id, dir_pictures, int(period))
-else:
-    output_images_to_telegram(token_bot, chat_id, dir_pictures)
