9b43a95a505cff21403e2037c1b45d62ce57ef6d
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-01-15 04:52:45 +0200

    Update collect_pictures.py

--- collect_pictures.py
+++ collect_pictures.py
@@ -6,15 +6,12 @@ from get_epic_nasa import get_epic_nasa
 
 
 dotenv.load_dotenv('venv/.env')
-id_for_example = '5eb87d47ffd86e000604b38a'
-token_nasa = os.getenv('NASA_TOKEN')
-token_bot = os.getenv('BOT_TOKEN')
-chat_id = os.getenv('CHAT_ID')
-dir_pictures = 'images'
+example_id = '5eb87d47ffd86e000604b38a'
+nasa_token = os.getenv('NASA_TOKEN')
 count = input('Input number of pictures from NASA, what should you get. For 1 picture, press Enter:')
-fetch_spacex_last_launch(id_for_example)
+fetch_spacex_last_launch(example_id)
 if count:
-    get_nasa_pictures(token_nasa, int(count))
+    get_nasa_pictures(nasa_token, int(count))
 else:
-    get_nasa_pictures(token_nasa)
-get_epic_nasa(token_nasa)
+    get_nasa_pictures(nasa_token)
+get_epic_nasa(nasa_token)
